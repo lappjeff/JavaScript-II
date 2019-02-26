8d293f9a49300071152be5f63bf8176eb543aa6e
unknown <lappjeff20@gmail.com>
2019-02-26 17:34:01 -0500

    callback.js MVP complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -61,18 +61,25 @@ multiplyNums(4, 4, function(sum) {
     console.log(`The multiplied numbers equal ${sum}`);
 })
 
+
+let item = 'Notebook'
+
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  return cb(item, list)
+
+  for (let i = 0; i < list.length; i++) {
+      if (list[i] === item) {
+          return cb(true);
+      }
+    }
+    return cb(false);
 }
 
-// contains('Pencil', items, function() {
-//     for (let i = 0; i < items.length; i++) {
-//         if (items[i] === 'Pencil') {
-//         }
-//     }
-// })
+
+contains(item, items, function (testCall) {
+    console.log(testCall)
+})
 
 
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -4,16 +4,15 @@
 function parent () {
     console.log('I contain a closure')
     const passItOn = 'I do not contain a closure'
-    debugger;
 
     function child () {
         console.log(passItOn)
-        debugger;
     }
     child();
 }
 
 parent();
+
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
